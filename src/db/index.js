b99d35cc21d6d1139ea12fd0c1c01f7c606835a5
Sunil Gautam <gautamsunil917@gmail.com>
2026-06-22 11:26:12 +0545

    monogodb Connection done with two different ways

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DATABASE_NAME } from "../contants.js";
+import { configDotenv } from "dotenv";
+
+configDotenv()
+
+const connectDB = async () => {
+    try {
+        const connectionResultinstance = await mongoose.connect(`${process.env.MONGO_URL}/${DATABASE_NAME}`)
+        console.log(`\n Mongodb Connected on ${connectionResultinstance.connection.host}  `)
+    } catch (error) {
+        console.error('MONGODB Connection Error', error)
+        process.exit(1)
+
+    }
+
+}
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-'asdasdasd'
+import express from 'express'
+import connectDB from "./db/index.js";
+import dns from 'dns'
+import { configDotenv } from "dotenv";
+
+configDotenv();
+dns.setServers(['8.8.8.8']);
+
+const app = express();
+
+
+connectDB();
+
+
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URL}/${DATABASE_NAME}`).then(() => {
+//             console.log("MonogDB Connected Sucessfully")
+//         })
+//         app.on('ERROR', (error) => {
+//             console.log('Couldnt listen to the app', error)
+//             throw error
+//         })
+
+//     } catch (error) {
+//         console.log('MONOGO Connection Error', error)
+//         throw error
+
+//     }
+// })()
