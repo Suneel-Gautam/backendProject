d83adf700f8f232e60146c214380df4fc86f1f6d
Sunil Gautam <gautamsunil917@gmail.com>
2026-07-03 23:21:02 +0545

    updated code for middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, size: '10mb' }));
+app.use(express.static('public'));
+app.use(cookieParser())
 
 export { app }
